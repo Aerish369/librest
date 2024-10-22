e4a6094673e8d272bc79b797518aa0a8dea91196
Aerish <aryalaerish369@gmail.com>
2024-10-22 23:24:56 +0545

    roles added in the user model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    LIBRARIAN = 'librarian'
+    MEMBER = 'member'
+    GUEST = 'guest'
+
+    ROLE_CHOICES = [
+        (LIBRARIAN, 'Librarian'),
+        (MEMBER, 'Member'),
+        (GUEST, 'Guest')
+    ]
+
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=MEMBER)
+
+    def __str__(self) -> str:
+        return self.username
