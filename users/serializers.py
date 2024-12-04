70d79b5e2d88423ef88b215bc30ca3cfb2ebc9bc
Aerish <aryalaerish369@gmail.com>
2024-12-04 22:00:04 +0545

    customized the current'user's serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,11 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['username', 'email', 'first_name', 'last_name', 'password'] 
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'role']
